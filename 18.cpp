9034a9ff6540c4c2f2c121e79a840d200dd3ec9b
vikaesska <malakovaviktoria671@gmail.com>
2026-03-14 00:00:07 +0300

    Create 18.cpp

---
+++ 18.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    int choice, size;
+    char texture;
+    
+    while(true) {
+        cout << "\n[ + ] Программа - \"Геометрические фигуры\"" << endl;
+        cout << "========================================" << endl;
+        cout << "[ 1 ] Линия" << endl;
+        cout << "[ 2 ] Квадрат" << endl;
+        cout << "[ 3 ] Прямоугольник" << endl;
+        cout << "[ 4 ] Треугольник" << endl;
+        cout << "[ 5 ] Решетка" << endl;
+        cout << "[ 6 ] Крестик" << endl;
+        cout << "[ 7 ] Плюс" << endl;
+        cout << "[ 8 ] Выйти" << endl;
+        cout << "========================================" << endl;
+        cout << "[ + ] Выберите фигуру: ";
+        cin >> choice;
+        
+        if(choice == 8) {
+            cout << "[ + ] Программа завершена!" << endl;
+            break;
+        }
+        
+        if(choice == 7) {
+            system("cls  clear");
+            
+            cout << "[ + ] Фигура: \"Плюс\"" << endl;
+            cout << "========================================" << endl;
+            cout << "[ + ] Размер: ";
+            cin >> size;
+            cout << "[ + ] Текстура: ";
+            cin >> texture;
+            
+            cout << "\n[ + ] Результат:\n" << endl;
+            
+            int center = size / 2;
+            
+            for(int i = 0; i < size; i++) {
+                for(int j = 0; j < size; j++) {
+                    if(i == center  j == center) {
+                        cout << texture;
+                    } else {
+                        cout << ".";
+                    }
+                    if(j < size - 1) cout << "  ";
+                }
+                cout << endl;
+            }
+            cout << endl;
+        } else {
+            cout << "[ - ] Выберите существующую фигуру!" << endl;
+        }
+    }
+    return 0;
+}
